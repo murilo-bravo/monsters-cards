9aa629a2e438d429ced901b579ca86e939bd2f63
Murilo Bravo <murilo.vibravo@gmail.com>
2022-08-09 10:03:37 -0300

    create components list and search-box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Component } from 'react'
 // import logo from './logo.svg'
 import './App.css'
+import CardList from './components/card-list/card-list.component'
+import SearchBox from './components/card-list/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -28,43 +30,42 @@ class App extends Component {
       )
   }
 
-  onSearchChange = (event) => {
+  onSearchChange = event => {
     //searchString will transform the target value to lower case
     const searchField = event.target.value.toLocaleLowerCase()
-    
+
     //setState to re-render the page with monsters filtered
     this.setState(() => {
-      return {searchField} //using a variable in a object field javaScript will atributte a key to a name of this variable and tha value of the value of this variable
+      return { searchField } //using a variable in a object field javaScript will atributte a key to a name of this variable and tha value of the value of this variable
     })
   }
 
-
   render() {
-    const {monsters, searchField} = this.state;
-    const {onSearchChange} = this;
+    const { monsters, searchField } = this.state
+    const { onSearchChange } = this
 
     //filteredMonster will return all monsters name that includes the target value into searchString
-    const filteredMonsters = monsters.filter((monster) => {
+    const filteredMonsters = monsters.filter(monster => {
       return monster.name.toLocaleLowerCase().includes(searchField)
-    });
+    })
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="search monsters"
-          //onChange is a event that's call everytime that do you make a change, like type a word
-          onChange={onSearchChange}
-        />
-        {filteredMonsters.map(monster => {
+        
+        {/* {filteredMonsters.map(monster => {
           return (
             //use 'key' on map to identify each result, set a id in every monster will make them unique
             <div key={monster.id}>
               <h1>{monster.name}</h1>
             </div>
           );
-        })};
+        })}; */}
+        <SearchBox 
+          onChangeHandler={onSearchChange}
+          placeholder='Search Monsters...'
+          className='search-box'
+          />
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
