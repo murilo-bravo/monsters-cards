0d7e295aa54ee18407c2aec3b5fc4d2390931ef8
Murilo Bravo <murilo.vibravo@gmail.com>
2022-08-19 09:17:52 -0300

    upload README

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-A monsters-rolodex that you can filter the monsters by their names, the project was build with react.js.
+# Galeria de Monstros
 
-All de data and images were collect from https://robohash.org/
+### Demo 📷
+<img src='/src/images/rolodex-img.PNG' /><br/>
 
+## 📖 Sobre 
+<hr>
+<p>
+  Uma galeria de cards de monstros que pode ser filtrada pelos nomes dos monstros. Todos os dados foram coletados de https://robohash.org/. <br/>
+  Realizei o deploy e coloquei na hospedagem, o site pode ser acessado pelo seguinte link: <a href="https://monsters.volppe.com.br/">Clique Aqui</a>
 
+</p><br/>
+
+## 📋 Pré-requisitos 
+<hr>
+<p>
+  Para começar é necessário instalar em sua máquina as ferramentas: <a href="https://git-scm.com/">Git</a>, <a href="https://nodejs.org/">Node,js</a>. Também deve ser intalado um editor de código como o <a href="https://code.visualstudio.com/">
+  VSCode</a>
+</p>
+
+#### ⚙️ Executando o Front End local
+• Clone esse repositório <br/> 
+• <code>npm intall</code> para instalar todas as dependências requeridas<br/>
+• <code>npm start</code> para rodar o projeto no servidor local<br/><br/>
+
+## 🛠️ Tecnologia
+
+• <a href="https://pt-br.reactjs.org/" target="_blank">React</a><br/>
+
+<hr>
+Made with 💙 by MURILO BRAVO 👋 <a href="https://www.linkedin.com/in/murilo-bravo-223154248/" target="_blank">Find me on LinkedIn</a><br/>
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react'
-// import logo from './logo.svg'
 import './App.css'
 import CardList from './components/card-list/card-list.component'
 import SearchBox from './components/search-box/search-box.component'
@@ -14,7 +13,7 @@ class App extends Component {
     }
   }
 
-  //always that have a component(class component) taht need leverage some kind API call to get the data to exhibit the UI, mus put into a componentDidMNount()
+  
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
@@ -31,12 +30,12 @@ class App extends Component {
   }
 
   onSearchChange = event => {
-    //searchString will transform the target value to lower case
+    
     const searchField = event.target.value.toLocaleLowerCase()
 
-    //setState to re-render the page with monsters filtered
+    
     this.setState(() => {
-      return { searchField } //using a variable in a object field javaScript will atributte a key to a name of this variable and tha value of the value of this variable
+      return { searchField } 
     })
   }
 
@@ -51,10 +50,10 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1 className='app-title'>Monsters</h1>
+        <h1 className='app-title'>Galeria de Monstros</h1>
         <SearchBox
           onChangeHandler={onSearchChange}
-          placeholder="Search Monsters..."
+          placeholder="Procure o Monstro;..."
           className="monsters-search-box"
         />
         <CardList monsters={filteredMonsters} />
